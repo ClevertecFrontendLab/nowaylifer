a72c609896de79e46f91200bb4c49aca98549835
Marat Bondarenko <nowaylifer@gmail.com>
2025-04-01 21:16:35 +0300

    chore(*): install inter font

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,3 +1,10 @@
+// @ts-expect-error seems like a bug https://github.com/fontsource/fontsource/issues/1038
+import '@fontsource-variable/inter';
+
 import { extendTheme } from '@chakra-ui/react';
 
-export const theme = extendTheme({});
+export const theme = extendTheme({
+    fonts: {
+        body: 'Inter Variable, sans-serif',
+    },
+});
