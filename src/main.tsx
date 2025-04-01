6310a45cceafc392c2450fcf430dbda37fbbd197
Marat Bondarenko <nowaylifer@gmail.com>
2025-04-01 21:09:29 +0300

    chore(*): setup chakra-ui & react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import './index.css';
-
-import { StrictMode } from 'react';
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 
-import App from '~/app/App.tsx';
-import { store } from '~/store/configure-store.ts';
+import App from './app/App';
 
 createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 );
