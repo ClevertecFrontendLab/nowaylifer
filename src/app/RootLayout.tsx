7bb35db168f3d91ca491f06cd24c7f1b74d00e35
Marat Bondarenko <nowaylifer@gmail.com>
2025-04-04 22:51:32 +0300

    feat(*): mark up bottom menu

--- src/app/RootLayout.tsx
+++ src/app/RootLayout.tsx
@@ -5,57 +5,64 @@ import { Button } from '~/shared/ui/Button';
 import { PenIcon } from '~/shared/ui/PenIcon';
 import { BookmarksStat, FriendsStat, LikesStat } from '~/shared/ui/Stats';
 import { AppHeader } from '~/widgets/AppHeader';
+import { BottomMenu } from '~/widgets/BottomMenu';
 import { SidebarMenu } from '~/widgets/SidebarMenu';
 
 export default function RootLayout() {
     return (
-        <Grid
-            templateAreas={`"header    header header"
-                            "side-menu main   side-profile"`}
-            gridTemplateRows='auto 1fr'
-            gridTemplateColumns='auto 1fr auto'
-            h='100dvh'
-        >
-            <GridItem area='header' zIndex='docked'>
-                <AppHeader />
-            </GridItem>
-            <GridItem area='side-menu' minH='full' hideBelow='lg' w='2xs'>
-                <SidebarMenu />
-            </GridItem>
-            <GridItem area='main' className='custom-scrollbar' overflowY='auto'>
-                <Box maxW={{ base: 'none', lg: '920px', '2xl': '1408px' }}>
-                    <Outlet />
-                </Box>
-            </GridItem>
-            <GridItem area='side-profile' hideBelow='xl'>
-                <VStack h='full' justifyContent='space-between' pt={4}>
-                    <VStack gap={6}>
-                        <BookmarksStat fontSize='md' value={185} />
-                        <FriendsStat fontSize='md' value={589} />
-                        <LikesStat fontSize='md' value={587} />
-                    </VStack>
-                    <HStack
-                        justify='center'
-                        boxSize='208px'
-                        bgGradient='radial(50% 50% at center, rgba(var(--chakra-colors-lime-300-rgb), 0.7), transparent)'
-                    >
-                        <Button
-                            _hover={{ bg: 'transparent' }}
-                            fontWeight='normal'
-                            iconSpacing={3}
-                            variant='ghost'
-                            fontSize='xs'
-                            topIcon={
-                                <Circle size={12} bg='black' color='lime.50'>
-                                    <PenIcon boxSize={6} />
-                                </Circle>
-                            }
+        <>
+            <Grid
+                templateAreas={`"header      header      header"
+                                "side-menu   main        side-profile"
+                                "bottom-menu bottom-menu bottom-menu"`}
+                gridTemplateRows='auto 1fr auto'
+                gridTemplateColumns='auto 1fr auto'
+                h='100dvh'
+            >
+                <GridItem area='header' zIndex='docked'>
+                    <AppHeader />
+                </GridItem>
+                <GridItem area='side-menu' minH='full' hideBelow='lg' w='2xs'>
+                    <SidebarMenu />
+                </GridItem>
+                <GridItem area='main' className='custom-scrollbar' overflowY='auto'>
+                    <Box maxW={{ base: 'none', lg: '920px', '2xl': '1408px' }}>
+                        <Outlet />
+                    </Box>
+                </GridItem>
+                <GridItem area='side-profile' hideBelow='xl'>
+                    <VStack h='full' justifyContent='space-between' pt={4}>
+                        <VStack gap={6}>
+                            <BookmarksStat fontSize='md' value={185} />
+                            <FriendsStat fontSize='md' value={589} />
+                            <LikesStat fontSize='md' value={587} />
+                        </VStack>
+                        <HStack
+                            justify='center'
+                            boxSize='208px'
+                            bgGradient='radial(50% 50% at center, rgba(var(--chakra-colors-lime-300-rgb), 0.7), transparent)'
                         >
-                            Записать рецепт
-                        </Button>
-                    </HStack>
-                </VStack>
-            </GridItem>
-        </Grid>
+                            <Button
+                                _hover={{ bg: 'transparent' }}
+                                fontWeight='normal'
+                                iconSpacing={3}
+                                variant='ghost'
+                                fontSize='xs'
+                                topIcon={
+                                    <Circle size={12} bg='black' color='lime.50'>
+                                        <PenIcon boxSize={6} />
+                                    </Circle>
+                                }
+                            >
+                                Записать рецепт
+                            </Button>
+                        </HStack>
+                    </VStack>
+                </GridItem>
+                <GridItem hideFrom='lg' area='bottom-menu'>
+                    <BottomMenu />
+                </GridItem>
+            </Grid>
+        </>
     );
 }
