9d8d81184df577af923164dd5b6953c928f21a03
Marat Bondarenko <nowaylifer@gmail.com>
2025-04-15 18:26:03 +0300

    feat(breadcrumbs): more flexible hook

--- src/shared/use-breadcrumbs.tsx
+++ src/shared/use-breadcrumbs.tsx
@@ -1,22 +1,25 @@
-import { UIMatch, useLocation, useMatches } from 'react-router';
+import { UIMatch, useMatches } from 'react-router';
 
-export const useBreadcrumbs = () => {
+type BreadcrumbData = { href: string; label: string };
+
+export const useBreadcrumbs = (): BreadcrumbData[] => {
     const matches = useMatches() as UIMatch<unknown, RouteHandle | undefined>[];
-    const { pathname } = useLocation();
     return matches
         .filter((m) => m.handle?.breadcrumb)
-        .map((m) => ({
-            active: m.pathname === pathname,
-            pathname: m.pathname,
-            label:
-                typeof m.handle!.breadcrumb === 'function'
-                    ? m.handle!.breadcrumb(m)
-                    : m.handle!.breadcrumb,
-        }));
+        .flatMap((m) => {
+            const breadcrumb = m.handle!.breadcrumb!;
+            const resolved = typeof breadcrumb === 'function' ? breadcrumb(m) : breadcrumb;
+            const arr = Array.isArray(resolved) ? resolved : [resolved];
+            return arr.map((v) => (typeof v === 'string' ? { label: v, href: m.pathname } : v));
+        });
 };
 
 export interface RouteHandle {
     breadcrumb?:
         | string
-        | ((match: UIMatch<unknown, RouteHandle | undefined>) => string | undefined);
+        | BreadcrumbData
+        | (string | BreadcrumbData)[]
+        | ((
+              match: UIMatch<unknown, RouteHandle | undefined>,
+          ) => string | BreadcrumbData | (string | BreadcrumbData)[]);
 }
