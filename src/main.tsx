c3e1bf5b04e662944dc6a97f063078a1c007e0ed
Marat Bondarenko <nowaylifer@gmail.com>
2025-04-23 22:58:42 +0300

    fix(*): skip motion animations in e2e

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import { MotionGlobalConfig } from 'framer-motion';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './app/App';
+import { isE2E } from './shared/lib/is-e2e';
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>,
 );
+
+if (isE2E()) {
+    MotionGlobalConfig.skipAnimations = true;
+}
