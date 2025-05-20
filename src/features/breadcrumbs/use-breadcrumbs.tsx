dd80d5c27cd5bd58a73790a04dd22f47755e8b58
Marat Bondarenko <nowaylifer@gmail.com>
2025-05-20 21:37:02 +0300

    fix(breadcrumbs): memoize use-breadcrumbs return value

--- src/features/breadcrumbs/use-breadcrumbs.tsx
+++ src/features/breadcrumbs/use-breadcrumbs.tsx
@@ -1,5 +1,6 @@
 import { runIfFn } from '@chakra-ui/utils';
 import { castArray, isString } from 'lodash-es';
+import { useMemo } from 'react';
 import { UIMatch, useLocation, useMatches } from 'react-router';
 
 import { BreadcrumbData, BreadcrumbDefinition, RouteBreadcrumb } from './types';
@@ -7,13 +8,21 @@ import { BreadcrumbData, BreadcrumbDefinition, RouteBreadcrumb } from './types';
 export function useBreadcrumbs(extraArg?: unknown): BreadcrumbData[] {
     const matches = useMatches() as UIMatch<unknown, RouteBreadcrumb<unknown, unknown>>[];
     const location = useLocation();
-    return matches
-        .filter((match) => match.handle?.crumb)
-        .flatMap((match) => {
-            const crumbDefinition = runIfFn(match.handle.crumb, { match, location, extraArg });
-            const definitions = castArray(crumbDefinition).filter(Boolean);
-            return definitions.map((def) => getBreadcrumbDataFromDefinition(def, match));
-        });
+    return useMemo(
+        () =>
+            matches
+                .filter((match) => match.handle?.crumb)
+                .flatMap((match) => {
+                    const crumbDefinition = runIfFn(match.handle.crumb, {
+                        match,
+                        location,
+                        extraArg,
+                    });
+                    const definitions = castArray(crumbDefinition).filter(Boolean);
+                    return definitions.map((def) => getBreadcrumbDataFromDefinition(def, match));
+                }),
+        [matches, location, extraArg],
+    );
 }
 
 const getBreadcrumbDataFromDefinition = (definition: BreadcrumbDefinition, match: UIMatch) =>
