6a418c4057dc2a9c384db442fe2aad72564a64e5
Marat Bondarenko <nowaylifer@gmail.com>
2025-06-07 14:04:32 +0300

    feat(blogs-page): add recipe slider

--- src/pages/blogs/blogs-page.tsx
+++ src/pages/blogs/blogs-page.tsx
@@ -1,6 +1,8 @@
 import { Heading } from '@chakra-ui/react';
 
 import { Main } from '~/shared/ui/main';
+import { Section, SectionHeading } from '~/shared/ui/section';
+import { NewRecipesSlider } from '~/widgets/new-recipes-slider';
 
 import { FavoriteBlogs } from './favorite-blogs';
 import { OtherBlogs } from './other-blogs';
@@ -17,5 +19,9 @@ export const BlogsPage = () => (
         </Heading>
         <FavoriteBlogs mb={{ base: 8, lg: 10 }} />
         <OtherBlogs />
+        <Section>
+            <SectionHeading mb={6}>Новые рецепты</SectionHeading>
+            <NewRecipesSlider />
+        </Section>
     </Main>
 );
